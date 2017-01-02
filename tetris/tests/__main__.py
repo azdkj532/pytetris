e81bd5c6025e51a50e31bf5bd6fe7b6484ea85bb
Inndy <inndy.tw@gmail.com>
2017-01-02 12:28:21 +0800

    Rename tests

--- tetris/tests/__main__.py
+++ tetris/tests/__main__.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 import unittest
 
 # import tests
-from blocks import *
+from test_blocks import *
 
 if __name__ == '__main__':
     unittest.main()
