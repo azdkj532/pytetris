4814b0b35bc64ce723f51353ce700720375e84e5
Inndy <inndy.tw@gmail.com>
2017-01-08 22:06:43 +0800

    Disable log from socketio

--- tetris/__main__.py
+++ tetris/__main__.py
@@ -5,6 +5,8 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 logging.basicConfig(level=logging.INFO)
+logging.getLogger('engineio').setLevel(logging.WARN)
+logging.getLogger('socketio').setLevel(logging.WARN)
 
 import tetris
 
