2a20eb19634967ccc7c8fd70710cb54dc07c66fc
Inndy <inndy.tw@gmail.com>
2017-01-08 16:11:26 +0800

    Add server, now can open room, join room

---
+++ tetris/server/server.py
@@ -0,0 +1,94 @@
+import os
+import logging
+import functools
+
+import eventlet; eventlet.monkey_patch()
+import socketio
+
+from bottle import Bottle, view, post, get, request, response, run, static_file
+
+from .game import Game
+from .user import User
+
+ASSETS_PATH = os.path.join(os.path.dirname(__file__), 'assets')
+VIEW_PATH = os.path.join(os.path.dirname(__file__), 'html')
+
+# setup bottle template path
+view = functools.partial(view, template_lookup=[VIEW_PATH])
+
+# create server objects
+sio = socketio.Server()
+bottle = Bottle()
+app = socketio.Middleware(sio, bottle)
+
+# dict({room_id: Game})
+games = {}
+
+# dict({user_id: User})
+users = {}
+
+@bottle.get('/')
+@view('index.html')
+def index():
+    pass
+
+@bottle.get('/assets/<file:path>')
+def assets(file):
+    return static_file(file, root=ASSETS_PATH)
+
+# connect and disconnect control
+
+@sio.on('connect', namespace='/game')
+def connect(sid, environ):
+    users[sid] = User(sid)
+    print('[+] User connected, sid = %s' % sid)
+
+@sio.on('disconnect', namespace='/game')
+def disconnect(sid):
+    game = users[sid].game
+    if game: game.remove_user(sid)
+    del users[sid]
+    print('[+] User disconnected, sid = %s' % sid)
+
+# room control
+
+@sio.on('create room', namespace='/game')
+def create_room(sid, *_):
+    user = users.get(sid, None)
+    if not user:
+        return
+
+    game = Game(sio)
+    sio.enter_room(sid, game.room_id, namespace='/game')
+
+    games[game.room_id] = game
+    game.add_user(user)
+
+    print('[+] User %s created room %s' % (sid, game.room_id))
+    sio.emit('room id', data=game.room_id, room=game.room_id, namespace='/game')
+
+@sio.on('join room', namespace='/game')
+def join_room(sid, room_id):
+    game = games.get(room_id, None)
+    user = users.get(sid, None)
+
+    if not game or not user or user.game:
+        return
+
+    game.add_user(user)
+    sio.enter_room(user.sid, game.room_id, namespace='/game')
+
+# user control
+@sio.on('set name', namespace='/game')
+def set_name(sid, name):
+    user = users.get(sid, None)
+    if not user:
+        return
+    old_name = user.name
+    user.name = name
+
+    if user.game:
+        user.game.broadcast('User %s renamed to %s' % (old_name, name))
+
+def start(host='0.0.0.0', port=8080):
+    run(app=app, host=host, port=port, server='eventlet')
---
+++ tetris/server/user.py
@@ -0,0 +1,16 @@
+import time
+import logging
+
+logger = logging.getLogger(__name__)
+
+class User(object):
+    def __init__(self, sid, name=None, game=None, board=None):
+        self.sid = sid
+        self.name = name or ('User_%s' % sid[:12])
+        self.game = None
+        self.board = None
+
+    def __repr__(self):
+        return 'User(sid=%r, game=%r, name=%r, board=%r)' % (
+            self.sid, self.game, self.name, self.board
+        )
