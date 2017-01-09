94d182f6213552e07020cc60db6172e486e63bb5
Inndy <inndy.tw@gmail.com>
2017-01-09 15:37:44 +0800

    Add Game.start and game worker

--- tetris/server/game.py
+++ tetris/server/game.py
@@ -1,14 +1,32 @@
 import os
 import logging
+from greenlet import GreenletExit
+from eventlet import greenthread
 
 logger = logging.getLogger(__name__)
 
 class Game(object):
-    def __init__(self, sio, owner, players={}, room_id=None):
+    def __init__(self, sio, owner, speed=0.75, players={}, room_id=None):
         self.sio = sio
         self.owner = owner
         self.players = players or {} # dict({user_id: User})
         self.room_id = room_id or os.urandom(16).hex()
+        self.worker = None # GreenThread
+        self.speed = speed # second(s)
+
+    def work(self): # background worker
+        self.broadcast('Hello players, this is message from worker!!!')
+
+    def worker_loop(self):
+        try:
+            while True:
+                self.work()
+                greenthread.sleep(self.speed)
+        except GreenletExit:
+            logger.debug('[*] R:%s - Worker exited' % self.room_id)
+
+    def start(self): # start game
+        self.worker = greenthread.spawn(self.worker_loop)
 
     def broadcast(self, message):
         logger.debug('[+] R:%s - Broadcast message: %r' % (self.room_id, message))
@@ -36,6 +54,9 @@ class Game(object):
         if user is self.owner:
             self.broadcast('Game owner leaved, this game is shutting down.')
             self.shutdown()
+            if self.worker:
+                self.worker.kill()
+                self.worker = None
 
     def add_user(self, user):
         if user.game:
--- tetris/server/server.py
+++ tetris/server/server.py
@@ -99,5 +99,12 @@ def set_name(sid, name):
         user.game.broadcast('User %s renamed to %s' % (old_name, name))
         logger.info('[+] U:%s - User renamed from %r to %r' % (sid, old_name, name))
 
+@sio.on('start game', namespace='/game')
+def start_game(sid, name):
+    user = users.get(sid, None)
+
+    if user and user.game and user is game.owner:
+        game.start()
+
 def start(host='0.0.0.0', port=8080):
     run(app=app, host=host, port=port, server='eventlet')
