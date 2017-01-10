a30db215e014465c89d9c6994574d754fd788fda
Squirrel <azdkj532@ntuosc.org>
2017-01-11 02:58:18 +0800

    refactor test

--- tetris/tests/test_game_board.py
+++ tetris/tests/test_game_board.py
@@ -3,28 +3,11 @@ from tetris import game
 from tetris.game.game_board import State
 import unittest
 
-class TestBoardHelperFunctions(unittest.TestCase):
+class TestBoardPublicAPI(unittest.TestCase):
     def setUp(self):
         self.gameboard = game.GameBoard()
         block = game.blocks.get_block('O')
         self.gameboard.current_block = block
-
-    def test_deposit_test(self):
-        block = game.blocks.get_block('O')
-        self.gameboard.current_block = block
-        self.gameboard.current_block_pos = (20, 0)
-        self.gameboard._make_deposit()
-        
-        self.assertEqual(self.gameboard._board[20][0], 1)
-        self.assertEqual(self.gameboard._board[20][0], 1)
-        self.assertEqual(self.gameboard._board[20][1], 1)
-        self.assertEqual(self.gameboard._board[20][2], 0)
-        self.assertEqual(self.gameboard._board[21][0], 1)
-        self.assertEqual(self.gameboard._board[21][1], 1)
-        self.assertEqual(self.gameboard._board[21][2], 0)
-        self.assertEqual(self.gameboard._board[22][0], 0)
-        self.assertEqual(self.gameboard._board[22][1], 0)
-        self.assertEqual(self.gameboard._board[22][2], 0)
 
     def test_block_next_tick(self):
         pos = self.gameboard.current_block_pos
@@ -38,54 +21,15 @@ class TestBoardHelperFunctions(unittest.TestCase):
         self.gameboard.current_block = block
         self.gameboard.down()
 
-        self.assertEqual(self.gameboard._board[21][5], 0)
-        self.assertEqual(self.gameboard._board[21][6], 0)
-        self.assertEqual(self.gameboard._board[21][7], 0)
-        self.assertEqual(self.gameboard._board[22][5], 1)
-        self.assertEqual(self.gameboard._board[22][6], 1)
-        self.assertEqual(self.gameboard._board[22][7], 0)
-        self.assertEqual(self.gameboard._board[23][5], 1)
-        self.assertEqual(self.gameboard._board[23][6], 1)
-        self.assertEqual(self.gameboard._board[23][7], 0)
-
-
-    def test_bottom_conflict_test(self):
-        block = game.blocks.get_block('O')
-        c_pos = (40, 0)
-        self.assertEqual(self.gameboard._conflict_detect(block, c_pos), State.OutOfBoard)
-
-        c_pos = (self.gameboard._height, 0)
-        self.assertEqual(self.gameboard._conflict_detect(block, c_pos), State.OutOfBoard)
-
-        c_pos = (10, 0)
-        self.assertEqual(self.gameboard._conflict_detect(block, c_pos), State.AllGreen)
-
-    def test_edge_conflict_test(self):
-        block = game.blocks.get_block('O')
-        c_pos = (10, self.gameboard.width)
-        self.assertEqual(self.gameboard._conflict_detect(block, c_pos), State.OutOfBoard)
-
-        c_pos = (10, -1)
-        self.assertEqual(self.gameboard._conflict_detect(block, c_pos), State.OutOfBoard)
-
-        c_pos = (10, self.gameboard.width-2)
-        self.assertEqual(self.gameboard._conflict_detect(block, c_pos), State.AllGreen)
-
-        block = game.blocks.get_block('I')
-        c_pos = (0, self.gameboard.width-1)
-        self.assertEqual(self.gameboard._conflict_detect(), State.AllGreen)
-
-    def test_block_conflict_test(self):
-        block = game.blocks.get_block('O')
-        pos = (20, 0)
-        self.gameboard.current_block = block
-        self.gameboard.current_block_pos = pos
-        self.gameboard._make_deposit(block, pos)
-
-        self.assertEqual(
-            self.gameboard._conflict_detect(block, pos),
-            State.ConflictWithBlock
-        )
+        self.assertEqual(self.gameboard._board[-3][5], 0)
+        self.assertEqual(self.gameboard._board[-3][6], 0)
+        self.assertEqual(self.gameboard._board[-3][7], 0)
+        self.assertEqual(self.gameboard._board[-2][5], 1)
+        self.assertEqual(self.gameboard._board[-2][6], 1)
+        self.assertEqual(self.gameboard._board[-2][7], 0)
+        self.assertEqual(self.gameboard._board[-1][5], 1)
+        self.assertEqual(self.gameboard._board[-1][6], 1)
+        self.assertEqual(self.gameboard._board[-1][7], 0)
 
     def test_rotate_success(self):
         block = game.blocks.get_block('I')
@@ -95,10 +39,10 @@ class TestBoardHelperFunctions(unittest.TestCase):
         self.assertEqual(str(self.gameboard.current_block), str(block.rotate('left')))
 
         self.gameboard.down()
-        self.assertEqual(self.gameboard._board[23][5], 1)
-        self.assertEqual(self.gameboard._board[23][6], 1)
-        self.assertEqual(self.gameboard._board[23][7], 1)
-        self.assertEqual(self.gameboard._board[23][8], 1)
+        self.assertEqual(self.gameboard._board[-1][5], 1)
+        self.assertEqual(self.gameboard._board[-1][6], 1)
+        self.assertEqual(self.gameboard._board[-1][7], 1)
+        self.assertEqual(self.gameboard._board[-1][8], 1)
 
     def test_rotate_exception(self):
         with self.assertRaises(ValueError):
@@ -142,6 +86,68 @@ class TestBoardHelperFunctions(unittest.TestCase):
         self.assertFalse(self.gameboard.is_gameover())
         self.gameboard._make_deposit()
         self.assertTrue(self.gameboard.is_gameover())
+
+
+class TestBoardHelperFunctions(unittest.TestCase):
+    def setUp(self):
+        self.gameboard = game.GameBoard()
+        block = game.blocks.get_block('O')
+        self.gameboard.current_block = block
+
+    def test_deposit_test(self):
+        block = game.blocks.get_block('O')
+        self.gameboard.current_block = block
+        self.gameboard.current_block_pos = (20, 0)
+        self.gameboard._make_deposit()
+
+        self.assertEqual(self.gameboard._board[20][0], 1)
+        self.assertEqual(self.gameboard._board[20][1], 1)
+        self.assertEqual(self.gameboard._board[20][2], 0)
+        self.assertEqual(self.gameboard._board[21][0], 1)
+        self.assertEqual(self.gameboard._board[21][1], 1)
+        self.assertEqual(self.gameboard._board[21][2], 0)
+        self.assertEqual(self.gameboard._board[22][0], 0)
+        self.assertEqual(self.gameboard._board[22][1], 0)
+        self.assertEqual(self.gameboard._board[22][2], 0)
+
+
+    def test_bottom_conflict_test(self):
+        block = game.blocks.get_block('O')
+        c_pos = (40, 0)
+        self.assertEqual(self.gameboard._conflict_detect(block, c_pos), State.OutOfBoard)
+
+        c_pos = (self.gameboard._height, 0)
+        self.assertEqual(self.gameboard._conflict_detect(block, c_pos), State.OutOfBoard)
+
+        c_pos = (10, 0)
+        self.assertEqual(self.gameboard._conflict_detect(block, c_pos), State.AllGreen)
+
+    def test_edge_conflict_test(self):
+        block = game.blocks.get_block('O')
+        c_pos = (10, self.gameboard.width)
+        self.assertEqual(self.gameboard._conflict_detect(block, c_pos), State.OutOfBoard)
+
+        c_pos = (10, -1)
+        self.assertEqual(self.gameboard._conflict_detect(block, c_pos), State.OutOfBoard)
+
+        c_pos = (10, self.gameboard.width-2)
+        self.assertEqual(self.gameboard._conflict_detect(block, c_pos), State.AllGreen)
+
+        block = game.blocks.get_block('I')
+        c_pos = (0, self.gameboard.width-1)
+        self.assertEqual(self.gameboard._conflict_detect(), State.AllGreen)
+
+    def test_block_conflict_test(self):
+        block = game.blocks.get_block('O')
+        pos = (20, 0)
+        self.gameboard.current_block = block
+        self.gameboard.current_block_pos = pos
+        self.gameboard._make_deposit(block, pos)
+
+        self.assertEqual(
+            self.gameboard._conflict_detect(block, pos),
+            State.ConflictWithBlock
+        )
 
     def test_line_erase(self):
         block = game.blocks.get_block('O')
